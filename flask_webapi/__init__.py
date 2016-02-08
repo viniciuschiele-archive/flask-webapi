68b5484cfb0910b3ed68e99520decc6aca08bb2d
Vinicius Chiele <vinicius.chiele@gmail.com>
2016-02-08 16:17:39 +0000

    Add import for APIError to make it easy to import by users

--- flask_webapi/__init__.py
+++ flask_webapi/__init__.py
@@ -3,6 +3,7 @@ from marshmallow import pre_load, pre_dump, post_load, post_dump, Schema, Valida
 from marshmallow.utils import missing
 
 from .api import WebAPI
+from .errors import APIError
 from .decorators import authenticator, permissions, content_negotiator, renderer, serializer, route
 from .views import ViewBase
 
