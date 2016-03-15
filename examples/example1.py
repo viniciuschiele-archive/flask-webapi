95d15caea2888f008630fb563191dcc0b6e03980
viniciuschiele <vinicius.chiele@gmail.com>
2016-03-15 15:13:54 +0000

    Improve example

--- examples/example1.py
+++ examples/example1.py
@@ -1,13 +1,14 @@
 from flask import Flask
 from flask_webapi import WebAPI
-from flask_webapi.exceptions import NotFound
+from flask_webapi.exceptions import BadRequest, NotFound
 from flask_webapi.decorators import route, param, serializer
 from flask_webapi.serializers import Serializer, StringField, IntegerField
 
 
 class User(object):
-    def __init__(self, username=None, first_name=None, last_name=None, age=None):
+    def __init__(self, username=None, password=None, first_name=None, last_name=None, age=None):
         self.username = username
+        self.password = password
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
@@ -15,12 +16,39 @@ class User(object):
 
 class UserSerializer(Serializer):
     username = StringField()
+    password = StringField(load_only=True)
     first_name = StringField()
     last_name = StringField()
-    age = IntegerField()
+    age = IntegerField(required=False)
+
+    def post_load(self, data, original_data):
+        return User(**data)
 
 
-users = [User(username='john.smith', first_name='John', last_name='Smith', age=30)]
+users = [
+    User(username='john.smith', password='1234', first_name='John', last_name='Smith', age=30)
+]
+
+
+@route('/users', methods=['POST'])
+@param('user', UserSerializer())
+@serializer(UserSerializer)
+def add_user(user):
+    if any([db_user for db_user in users if db_user.username == user.username]):
+        raise BadRequest('User already exists: ' + user.username)
+
+    users.append(user)
+    return user
+
+
+@route('/users/<username>', methods=['DELETE'])
+def delete_user(username):
+    found = [user for user in users if user.username == username]
+
+    if not found:
+        raise NotFound('User not found: ' + username)
+
+    users.remove(found[0])
 
 
 @route('/users')
@@ -39,11 +67,14 @@ def get_user(username):
         if username == user.username:
             return user
 
-    return NotFound()
+    raise NotFound('User not found: ' + username)
+
 
 if __name__ == '__main__':
     app = Flask(__name__)
     api = WebAPI(app)
+    api.add_view(add_user)
+    api.add_view(delete_user)
     api.add_view(get_user)
     api.add_view(get_users)
     app.run()
