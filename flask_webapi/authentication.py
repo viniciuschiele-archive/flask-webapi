76642bf61960dc05b9ba1423803ef66e71bdf6db
viniciuschiele <vinicius.chiele@gmail.com>
2016-04-17 11:05:12 +0100

    Add an abstract http authenticator

--- flask_webapi/authentication.py
+++ flask_webapi/authentication.py
@@ -49,36 +49,72 @@ class Authenticator(metaclass=ABCMeta):
         """
 
 
-class BasicAuthenticator(Authenticator):
+class HttpAuthenticator(Authenticator):
     """
-    HTTP Basic authentication against username and password.
+    Base authenticator based on HTTP Authorization header.
     """
 
     def authenticate(self):
         """
-        Returns the user authenticated if a correct username and password have been supplied
-        using HTTP Basic authentication, otherwise returns `None`.
+        Returns the user authenticated if a correct Authorization header has been supplied,
+        otherwise returns `None`.
         """
-        auth = request.authorization
+        auth = request.headers.get('Authorization', '')
 
         if not auth:
             return None
 
-        if auth.type.lower() != 'basic':
+        # expected auth type + auth info
+        # e.g basic a8s7d6a8sd:aASD%SAa5d6s
+        separator = auth.find(auth, ' ')
+        if separator == -1:
             return None
 
-        user_auth = self.authenticate_credentials(auth.username, auth.password)
+        auth_type = auth.substring(auth, 0, separator)
+        auth_info = auth.substring(auth, separator + 1)
+
+        return self._authenticate_header(auth_type, auth_info)
+
+    def _authenticate_header(self, auth_type, auth_info):
+        """
+        Authenticates the given authorization header.
+        :param auth_type: The authorization type.
+        :param auth_info: The authorization info.
+        :return: A two-tuple of (user, auth) or `None`.
+        """
+        raise NotImplementedError()
+
+
+class BasicAuthenticator(HttpAuthenticator):
+    """
+    HTTP Basic authentication against username and password.
+    """
+
+    def _authenticate_header(self, auth_type, auth_info):
+        """
+        Returns the user authenticated if a correct username and password have been supplied
+        using HTTP Basic authentication, otherwise returns `None`.
+        """
+        if auth_type != 'basic':
+            return None
+
+        try:
+            username, password = auth_info.split(':')
+        except ValueError:
+            raise AuthenticationFailed('Invalid basic header. Credentials not correctly base64 encoded.')
+
+        user_auth = self._authenticate_credentials(username, password)
 
         if user_auth is None:
-            raise AuthenticationFailed('Invalid username/password.')
+            raise AuthenticationFailed()
 
         return user_auth
 
-    def authenticate_credentials(self, username, password):
+    def _authenticate_credentials(self, username, password):
         """
         Authenticates the given username and password.
         :param username: The username to be authenticated.
-        :param password: The username to be authenticated.
+        :param password: The password to be authenticated.
         :return: A two-tuple of (user, auth) or `None`.
         """
         raise NotImplementedError()
